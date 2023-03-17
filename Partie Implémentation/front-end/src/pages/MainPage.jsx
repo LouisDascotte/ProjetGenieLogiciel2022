5069d25844ea7bb236fe273f2d9a2bcc573d2516
GoddWilll <202543@umons.ac.be>
2023-03-17 19:09:53 +0100

    Finally managed to make the Portfolio selection responsive ( lmao )

--- Partie Implémentation/front-end/src/pages/MainPage.jsx
+++ Partie Implémentation/front-end/src/pages/MainPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { useState } from "react";
-import {Stack, Box, Grid, Divider} from '@mui/material';
+import { useState, useEffect,  forceUpdate } from "react";
+import {Stack, Box, Grid, Divider, TextField} from '@mui/material';
 import PortfolioPlaceHolder from '../components/PortfolioPlaceHolder';
 import TopMenu from '../components/TopMenu';
 import PortfolioMainGraph from '../components/PortfolioMainGraph';
@@ -14,96 +14,135 @@ import ElectricMeterOutlinedIcon from '@mui/icons-material/ElectricMeterOutlined
 import AssignmentIndOutlinedIcon from '@mui/icons-material/AssignmentIndOutlined';
 import {Select, FormControl, InputLabel } from '@mui/material';
 import {  Link} from 'react-router-dom';
+import { useForceUpdate } from '../components/hooks/useForceUpdate';
+
+
+
+
 
 
 const MainPage = () => {
-  // hardcoded const in order to test the "create portfolio message"
-  const hasCreatedPortfolios = true; 
-  const pageAddress = "/main-page";
-  const pageName = "General overview";
-
-  const { collapseSidebar } = useProSidebar(); 
+    // hardcoded const in order to test the "create portfolio message"
+    const [hasSelectedPortfolio, setHasSelectedPortfolio] = useState(false);
     const [page, setPage] = useState("");
-    const changePage = (event) => {
-        setPage(event.target.value);
-        
+
+
+    const pageAddress = "/main-page";
+    const pageName = "General overview";
+
+    const { collapseSidebar } = useProSidebar(); 
+
+
+    const handleChange = (e) => {
+        const{value} = e.target; 
+        setPage(value);
     }
-  return (
-    <Stack direction='row' sx={{width:"100%", height:"100%", position:'fixed'}}>
-      <Sidebar style={{ height:"100%"}}>
-            <Menu>
-                <MenuItem
-                    icon= {<MenuOutlinedIcon/>}
-                    onClick={() => {
-                        collapseSidebar();
-                    }}
-                    style={{ textAlign: 'center'}}>
-                        
-                        
-                        
-                </MenuItem>
-                <MenuItem icon={<WalletOutlinedIcon/>}>
-                    <Box sx={{ minWidth: 120 }}>
-                            <FormControl fullWidth size="small" margin="normal">
-                            <InputLabel margin="normal" id="select-portfolio-label">Portfolio :</InputLabel>
-                                <Select
-                                labelId="portfolio-select-label"
-                                id="portfolio-select"
-                                value={page}
-                                label="Portfolio :"
-                                onChange={changePage}
-                                >
-                                <MenuItem value={"/"}>None</MenuItem>
-                                <MenuItem value={"/portfolio1"}>Porfolio 1</MenuItem>                    
-                                </Select>
-                            </FormControl>
-                        </Box>
+
+    useEffect(()=> {
+        console.log("Value of page :", page);
+        if (page !== ""){
+            setHasSelectedPortfolio(true);
+        }
+    })
+
+    return (
+        <Stack direction='row' sx={{width:"100%", height:"100%", position:'fixed'}}>
+            <Sidebar style={{ height:"100%"}}>
+                <Stack>
+                    <Menu>
+                        <MenuItem
+                            icon= {<MenuOutlinedIcon/>}
+                            onClick={() => {
+                                collapseSidebar();
+                            }}
+                            style={{ textAlign: 'center'}}>
+                        </MenuItem>
+
+                        <MenuItem icon={<WalletOutlinedIcon/>}>
+                            <Menu>
+                                <Box sx={{ minWidth: 120 }}>
+                                <FormControl fullWidth size="small" margin="normal">
+                                <InputLabel margin="normal" id="select-portfolio-label">Portfolio :</InputLabel>
+                                    <Select
+                                    labelId="portfolio-select-label"
+                                    id="portfolio-select"
+                                    value={page}
+                                    label="Portfolio :"
+                                    onChange={handleChange}
+                                    >
+                                    <MenuItem value="/">
+                                        <em>None</em>
+                                    </MenuItem>
+                                    <MenuItem value={"data1"}>Portfolio 1</MenuItem>
+                                    <MenuItem value={"data2"}>Portfolio 2</MenuItem>                    
+                                    </Select>
+                                </FormControl>   
+                                </Box>
+                            </Menu>
+                        </MenuItem>
+                    </Menu>
+                </Stack>
+                <Menu>      
+                    <MenuItem onClick={()=>{}}> 
+                        <Divider/>
                     </MenuItem>
-                <MenuItem onClick={()=>{}}> 
-                    <Divider/>
-                </MenuItem>
-                <Link to="/main-page" className='link' >
-                    <MenuItem icon={<VisibilityOutlinedIcon />} onClick={()=>{}}>
-                        General overview
-                    </MenuItem>
-                </Link>
-                <Link to="/manage-portfolios" className='link'>
-                    <MenuItem icon={<AccountBalanceWalletOutlinedIcon />} onClick={()=>{}}>
-                        Manage portfolios
-                    </MenuItem>
-                </Link>
-                <Link to="/manage-meters" className='link'>
-                    <MenuItem icon={<ElectricMeterOutlinedIcon />}>
-                        Manage meters
-                    </MenuItem>
-                </Link>
-                <Link to="/manage-contracts" className='link'>
-                    <MenuItem icon={<AssignmentIndOutlinedIcon />}>
-                        Manage contracts
-                    </MenuItem>
-                </Link>
-                <Link to="/manage-invoices" className='link'>
-                    <MenuItem icon={<ReceiptOutlinedIcon />} >
-                        Manage invoices
-                    </MenuItem>
-                </Link>
-            </Menu>
-        </Sidebar>
-      <Stack sx={{display:'flex', width:"100%"}}>
-        
-        <TopMenu pageAddress={pageAddress} pageName = {pageName}/>
-        
-        <Stack direction='row' justifyContent='center' paddingTop='10%'>
-          { // if the user created a portfolio, print 'Portfolio graphic', otherwise print the creation message
-          // 'Portfolio graphic' replaces an actual portfolio infographic for now
-          hasCreatedPortfolios ? <PortfolioMainGraph/> : <PortfolioPlaceHolder/>
-          }
+                    <Link to="/main-page" className='link' >
+                        <MenuItem icon={<VisibilityOutlinedIcon />} onClick={()=>{}}>
+                            General overview
+                        </MenuItem>
+                    </Link>
+                    <Link to="/manage-portfolios" className='link'>
+                        <MenuItem icon={<AccountBalanceWalletOutlinedIcon />} onClick={()=>{}}>
+                            Manage portfolios
+                        </MenuItem>
+                    </Link>
+                    <Link to="/manage-meters" className='link'>
+                        <MenuItem icon={<ElectricMeterOutlinedIcon />}>
+                            Manage meters
+                        </MenuItem>
+                    </Link>
+                    <Link to="/manage-contracts" className='link'>
+                        <MenuItem icon={<AssignmentIndOutlinedIcon />}>
+                            Manage contracts
+                        </MenuItem>
+                    </Link>
+                    <Link to="/manage-invoices" className='link'>
+                        <MenuItem icon={<ReceiptOutlinedIcon />} >
+                            Manage invoices
+                        </MenuItem>
+                    </Link>
+                </Menu>
+            </Sidebar>
+        <Stack sx={{display:'flex', width:"100%"}}>  
+            <TopMenu pageAddress={pageAddress} pageName = {pageName}/>
+            <Stack direction='row' justifyContent='center' paddingTop='10%'>
+                { // if the user created a portfolio, print 'Portfolio graphic', otherwise print the creation message
+                // 'Portfolio graphic' replaces an actual portfolio infographic for now
+                hasSelectedPortfolio ? <PortfolioMainGraph portfolio={page}/> : <PortfolioPlaceHolder/>
+                }
+            </Stack>
         </Stack>
-      </Stack>
     </Stack>
-  );
+    );
 }
   
 
 
 export default MainPage
+
+/**<FormControl fullWidth size="small" margin="normal">
+                                <InputLabel margin="normal" id="select-portfolio-label">Portfolio :</InputLabel>
+                                    <Select
+                                    labelId="portfolio-select-label"
+                                    id="portfolio-select"
+                                    value={page}
+                                    label="Portfolio :"
+                                    onChange={handleChange}
+                                    >
+                                    <MenuItem value="">
+                                        <em>None</em>
+                                    </MenuItem>
+                                    <MenuItem value={1}>Portfolio 1</MenuItem>
+                                    <MenuItem value={2}>Portfolio 2</MenuItem>                    
+                                    </Select>
+                                </FormControl> */

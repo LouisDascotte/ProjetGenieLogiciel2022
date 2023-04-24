b3111798a2cc5815512aded51cb21174e41b5ba4
GoddWilll <202543@umons.ac.be>
2023-04-24 22:54:21 +0200

    Mise à jour de la vérification de JWT.

--- Partie Implémentation/front-end/client_app/src/utils/private_route/index.js
+++ Partie Implémentation/front-end/client_app/src/utils/private_route/index.js
@@ -8,6 +8,19 @@ const JWT_VALIDATION_URL = "http://localhost:8080/api/"
 const PrivateRoute = ({children}) => {
   //const [jwt, setJwt] = useLocalState("", "jwt");
   const jwt = localStorage.getItem("jwt");
+  console.log(jwt);
+  React.useEffect(()=>{
+    const response = axios.get(JWT_VALIDATION_URL + "auth/expired_token", {
+      headers : {
+        "Content-Type":"application/json",
+        "Access-Control-Allow-Origin":true,
+      }, params : {
+        token : jwt
+      }
+    }).then(response => {
+      console.log(response.data)
+    })
+  })
   /*if (jwt) {
     const response = axios.get(JWT_VALIDATION_URL, {headers : {"Content-Type":"application/json",
     "Authorization" : `Bearer ${jwt}`,
